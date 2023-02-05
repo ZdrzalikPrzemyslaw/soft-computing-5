af61532338f57f3546edbc817755c5ff48736891
zdrzalikprzemyslaw <zdrzalikprzemyslaw@gmail.com>
2023-02-05 19:04:26 +0100

    bez siatki

--- python/PSNR.py
+++ python/PSNR.py
@@ -3,6 +3,8 @@ import numpy as np
 from math import log10, sqrt
 import csv
 
+from matplotlib import pyplot as plt
+
 files = ['01.bmp', '02.bmp', '03.bmp', '04.bmp', '05.bmp', '06.bmp', '07.bmp', '08.bmp', ]
 dirs = ['./out/1/', './out/2/', './out/3/', './out/4/', './out/8/', './out/16/', './out/32/', './out/64/', './out/128/', ]
 
@@ -18,6 +20,22 @@ def PSNR(original, compressed):
     psnr = 10 * log10(max_pixel**2 / mse)
     return psnr
 
+def plot_many(values):
+    fig, ax = plt.subplots()
+
+    # We need to draw the canvas, otherwise the labels won't be positioned and
+    # won't have values yet.
+    fig.canvas.draw()
+
+    labels = [ '', '1','2','3','4','8','16','32','64','128']
+
+    for i in values:
+        ax.plot(i,  **{'marker': 'o'})
+
+    ax.set_xticklabels(labels)
+
+    plt.show()
+
 
 outputs = []
 psnr_values = []
@@ -32,6 +50,8 @@ for idx, j in enumerate(files):
         outputs.append("PSNR OF " + or_str + " and " + comp_str + " = " + PSNR(original, compressed).__str__())
 
 psnr_values = np.asarray(psnr_values)
+
+plot_many(psnr_values)
 
 with open("PSNR.csv", "w+") as my_csv:
     csvWriter = csv.writer(my_csv, delimiter=';',
